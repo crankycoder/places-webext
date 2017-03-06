868d327853c95b7466f5511293139b4611be20da
Victor Ng <victor@crankycoder.com>
2017-03-06 14:34:43 -0500

    removed old example ProductTable classes

--- extension/popup/groupspanel.js
+++ extension/popup/groupspanel.js
@@ -8,258 +8,99 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var ProductCategoryRow = function (_React$Component) {
-  _inherits(ProductCategoryRow, _React$Component);
+var Site = function (_React$Component) {
+    _inherits(Site, _React$Component);
 
-  function ProductCategoryRow() {
-    _classCallCheck(this, ProductCategoryRow);
+    function Site() {
+        _classCallCheck(this, Site);
 
-    return _possibleConstructorReturn(this, (ProductCategoryRow.__proto__ || Object.getPrototypeOf(ProductCategoryRow)).apply(this, arguments));
-  }
-
-  _createClass(ProductCategoryRow, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "tr",
-        null,
-        React.createElement(
-          "th",
-          { colSpan: "2" },
-          this.props.category
-        )
-      );
+        return _possibleConstructorReturn(this, (Site.__proto__ || Object.getPrototypeOf(Site)).apply(this, arguments));
     }
-  }]);
 
-  return ProductCategoryRow;
-}(React.Component);
-
-var ProductRow = function (_React$Component2) {
-  _inherits(ProductRow, _React$Component2);
-
-  function ProductRow() {
-    _classCallCheck(this, ProductRow);
-
-    return _possibleConstructorReturn(this, (ProductRow.__proto__ || Object.getPrototypeOf(ProductRow)).apply(this, arguments));
-  }
-
-  _createClass(ProductRow, [{
-    key: "render",
-    value: function render() {
-      var name = this.props.product.stocked ? this.props.product.name : React.createElement(
-        "span",
-        { style: { color: 'red' } },
-        this.props.product.name
-      );
-      return React.createElement(
-        "tr",
-        null,
-        React.createElement(
-          "td",
-          null,
-          name
-        ),
-        React.createElement(
-          "td",
-          null,
-          this.props.product.price
-        )
-      );
-    }
-  }]);
-
-  return ProductRow;
-}(React.Component);
-
-var ProductTable = function (_React$Component3) {
-  _inherits(ProductTable, _React$Component3);
-
-  function ProductTable() {
-    _classCallCheck(this, ProductTable);
-
-    return _possibleConstructorReturn(this, (ProductTable.__proto__ || Object.getPrototypeOf(ProductTable)).apply(this, arguments));
-  }
-
-  _createClass(ProductTable, [{
-    key: "render",
-    value: function render() {
-      var rows = [];
-      var lastCategory = null;
-      this.props.products.forEach(function (product) {
-        if (product.category !== lastCategory) {
-          rows.push(React.createElement(ProductCategoryRow, { category: product.category, key: product.category }));
+    _createClass(Site, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "li",
+                { className: "tab" },
+                React.createElement("img", { alt: "", className: "tab-icon", src: "{this.props.site.favicon}" }),
+                React.createElement(
+                    "span",
+                    { className: "tab-title" },
+                    this.props.site.title,
+                    "|",
+                    this.props.site.url
+                )
+            );
         }
-        rows.push(React.createElement(ProductRow, { product: product, key: product.name }));
-        lastCategory = product.category;
-      });
-      return React.createElement(
-        "table",
-        null,
-        React.createElement(
-          "thead",
-          null,
-          React.createElement(
-            "tr",
-            null,
-            React.createElement(
-              "th",
-              null,
-              "Name"
-            ),
-            React.createElement(
-              "th",
-              null,
-              "Price"
-            )
-          )
-        ),
-        React.createElement(
-          "tbody",
-          null,
-          rows
-        )
-      );
-    }
-  }]);
+    }]);
 
-  return ProductTable;
+    return Site;
 }(React.Component);
 
-var SearchBar = function (_React$Component4) {
-  _inherits(SearchBar, _React$Component4);
+var SiteList = function (_React$Component2) {
+    _inherits(SiteList, _React$Component2);
 
-  function SearchBar() {
-    _classCallCheck(this, SearchBar);
+    function SiteList() {
+        _classCallCheck(this, SiteList);
 
-    return _possibleConstructorReturn(this, (SearchBar.__proto__ || Object.getPrototypeOf(SearchBar)).apply(this, arguments));
-  }
-
-  _createClass(SearchBar, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "form",
-        null,
-        React.createElement("input", { type: "text", placeholder: "Search..." }),
-        React.createElement(
-          "p",
-          null,
-          React.createElement("input", { type: "checkbox" }),
-          ' ',
-          "Only show products in stock"
-        )
-      );
+        return _possibleConstructorReturn(this, (SiteList.__proto__ || Object.getPrototypeOf(SiteList)).apply(this, arguments));
     }
-  }]);
 
-  return SearchBar;
+    _createClass(SiteList, [{
+        key: "render",
+        value: function render() {
+            var rows = [];
+            this.props.sites.forEach(function (thisSite) {
+                rows.push(React.createElement(Site, { site: thisSite, key: thisSite.url }));
+            });
+
+            return React.createElement(
+                "ul",
+                { className: "tab-list" },
+                rows
+            );
+        }
+    }]);
+
+    return SiteList;
 }(React.Component);
 
-var Site = function (_React$Component5) {
-  _inherits(Site, _React$Component5);
+var SuggestedLinks = function (_React$Component3) {
+    _inherits(SuggestedLinks, _React$Component3);
 
-  function Site() {
-    _classCallCheck(this, Site);
+    function SuggestedLinks() {
+        _classCallCheck(this, SuggestedLinks);
 
-    return _possibleConstructorReturn(this, (Site.__proto__ || Object.getPrototypeOf(Site)).apply(this, arguments));
-  }
-
-  _createClass(Site, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "li",
-        { className: "tab" },
-        React.createElement("img", { alt: "", className: "tab-icon", src: "{this.props.site.favicon}" }),
-        React.createElement(
-          "span",
-          { className: "tab-title" },
-          this.props.site.title,
-          "|",
-          this.props.site.url
-        )
-      );
+        return _possibleConstructorReturn(this, (SuggestedLinks.__proto__ || Object.getPrototypeOf(SuggestedLinks)).apply(this, arguments));
     }
-  }]);
 
-  return Site;
-}(React.Component);
+    _createClass(SuggestedLinks, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "ul",
+                { className: "group expanded active" },
+                React.createElement(
+                    "span",
+                    { className: "group-title" },
+                    "Suggested Links"
+                ),
+                React.createElement(SiteList, { sites: SITES })
+            );
+        }
+    }]);
 
-var TabList = function (_React$Component6) {
-  _inherits(TabList, _React$Component6);
-
-  function TabList() {
-    _classCallCheck(this, TabList);
-
-    return _possibleConstructorReturn(this, (TabList.__proto__ || Object.getPrototypeOf(TabList)).apply(this, arguments));
-  }
-
-  _createClass(TabList, [{
-    key: "render",
-    value: function render() {
-      var rows = [];
-      this.props.sites.forEach(function (thisSite) {
-        rows.push(React.createElement(Site, { site: thisSite, key: thisSite.url }));
-      });
-
-      /*
-                <li className="tab">
-                    <img alt="" className="tab-icon" src="https://www.google.com/favicon.ico"/>
-                    <span className="tab-title">Amazon - AC Charger Power Adapter For ASUS Chromebook Flip C100</span>
-                </li>
-                <li className="tab">
-                    <img alt="" className="tab-icon" src="https://www.google.com/favicon.ico"/>
-                    <span className="tab-title">ReactDOM - React</span>
-                </li>
-      */
-
-      return React.createElement(
-        "ul",
-        { className: "tab-list" },
-        rows
-      );
-    }
-  }]);
-
-  return TabList;
-}(React.Component);
-
-var SuggestedLinks = function (_React$Component7) {
-  _inherits(SuggestedLinks, _React$Component7);
-
-  function SuggestedLinks() {
-    _classCallCheck(this, SuggestedLinks);
-
-    return _possibleConstructorReturn(this, (SuggestedLinks.__proto__ || Object.getPrototypeOf(SuggestedLinks)).apply(this, arguments));
-  }
-
-  _createClass(SuggestedLinks, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "ul",
-        { className: "group expanded active" },
-        React.createElement(
-          "span",
-          { className: "group-title" },
-          "Suggested Links"
-        ),
-        React.createElement(TabList, { sites: SITES })
-      );
-    }
-  }]);
-
-  return SuggestedLinks;
+    return SuggestedLinks;
 }(React.Component);
 
 var SITES = [{ favicon: "https://www.amazon.com/favicon.ico",
-  title: "Amazon - AC Charger Power Adapter For ASUS Chromebook Flip C100",
-  url: "https://www.amazon.ca/Charger-Chromebook-10-1-Inch-Convertible-Touchscreen/dp/B01IXZE6Z8/ref=sr_1_1?ie=UTF8&qid=1488569483&sr=8-1&keywords=chromebook+asus+adapter" }, { favicon: "https://code.facebook.com/favicon.ico",
-  title: 'Thinking in React - React',
-  url: "https://facebook.github.io/react/docs/thinking-in-react.html" }, { favicon: "https://www.reddit.com/favicon.ico",
-  title: 'top scoring links - Reddit',
-  url: "https://www.reddit.com/top/" }];
+    title: "Amazon - AC Charger Power Adapter For ASUS Chromebook Flip C100",
+    url: "https://www.amazon.ca/Charger-Chromebook-10-1-Inch-Convertible-Touchscreen/dp/B01IXZE6Z8/ref=sr_1_1?ie=UTF8&qid=1488569483&sr=8-1&keywords=chromebook+asus+adapter" }, { favicon: "https://code.facebook.com/favicon.ico",
+    title: 'Thinking in React - React',
+    url: "https://facebook.github.io/react/docs/thinking-in-react.html" }, { favicon: "https://www.reddit.com/favicon.ico",
+    title: 'top scoring links - Reddit',
+    url: "https://www.reddit.com/top/" }];
 
 ReactDOM.render(React.createElement(SuggestedLinks, { sites: SITES }), document.getElementById('container'));
 
--- extension/popup/groupspanel.jsx
+++ extension/popup/groupspanel.jsx
@@ -1,66 +1,3 @@
-
-class ProductCategoryRow extends React.Component {
-  render() {
-    return <tr><th colSpan="2">{this.props.category}</th></tr>;
-  }
-}
-
-class ProductRow extends React.Component {
-  render() {
-    var name = this.props.product.stocked ?
-      this.props.product.name :
-      <span style={{color: 'red'}}>
-        {this.props.product.name}
-      </span>;
-    return (
-      <tr>
-        <td>{name}</td>
-        <td>{this.props.product.price}</td>
-      </tr>
-    );
-  }
-}
-
-class ProductTable extends React.Component {
-  render() {
-    var rows = [];
-    var lastCategory = null;
-    this.props.products.forEach(function(product) {
-      if (product.category !== lastCategory) {
-        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
-      }
-      rows.push(<ProductRow product={product} key={product.name} />);
-      lastCategory = product.category;
-    });
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
-    );
-  }
-}
-
-class SearchBar extends React.Component {
-  render() {
-    return (
-      <form>
-        <input type="text" placeholder="Search..." />
-        <p>
-          <input type="checkbox" />
-          {' '}
-          Only show products in stock
-        </p>
-      </form>
-    );
-  }
-}
-
 class Site extends React.Component {
     render() {
         return (
@@ -72,7 +9,7 @@ class Site extends React.Component {
     }
 }
 
-class TabList extends React.Component {
+class SiteList extends React.Component {
   render() {
       var rows = [];
       this.props.sites.forEach(function(thisSite) {
@@ -92,7 +29,7 @@ class SuggestedLinks extends React.Component {
     return (
         <ul className="group expanded active">
             <span className="group-title">Suggested Links</span>
-            <TabList sites={SITES} />
+            <SiteList sites={SITES} />
         </ul>
     );
   }
