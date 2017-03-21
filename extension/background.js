9f2e6bfdde0471b5f331d351a815c9d05193f395
Victor Ng <victor@crankycoder.com>
2017-03-21 16:24:06 -0400

    deleted dead code

--- extension/background.js
+++ extension/background.js
@@ -15,16 +15,3 @@ function handleMessage(request, sender, sendResponse) {
 }
 
 browser.runtime.onMessage.addListener(handleMessage);
-
-/*
-browser.placesdb.query({query: "select id, visit_type from moz_historyvisits limit 10", params: ['id', 'visit_type']}).then(
-    function(results) {
-        for (var i = 0; i < results.length; i++) {
-            console.log("Row foo: " + results[i].id + " | " + results[i].visit_type);
-        }
-    },
-    function(reason) {
-        console.log("failure reason: " + reason); // Fail!
-    }
-);
-*/
