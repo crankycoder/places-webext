b43d1318d968614de3d9f7998be98e5eb61dbc9c
Victor Ng <victor@crankycoder.com>
2017-03-22 17:59:25 -0400

    basic message pass back from background script to UI now works

--- extension/background.js
+++ extension/background.js
@@ -1,35 +1,113 @@
-if (Redux === undefined) {
-    console.log("Redux is not loaded yet");
-} else {
-    console.log("Redux is available!");
-
-}
-
 const store = Redux.createStore(heatmapApp);
+var Provider = ReactRedux.Provider;
 
-console.log("background.js heatmapApp: " + heatmapApp);
-console.log("background.js store: " + store);
+var DATA_LOADED = false;
 
-var loaded = false;
+function loadGuard() {
+    if (DATA_LOADED) {
+        console.log("Data is already loaded.");
+        return 'DATA_READY';
+    }
+    DATA_LOADED = true;
+    // We want to load the data once and only once
+    console.log("Data load requested to the popup");
+    loadHeatmap();
+    return 'DATA_LOADING';
+}
 
 function handleMessage(request, sender, sendResponse) {
-    // We have to wait for the first click to invoke the Redux code because JS
-    // is stupid and we have no guarantees about which code is actually loaded
-    console.log("In background.js .. Redux namespace: " + Redux);
-}
-
-function oldhandleMessage(request, sender, sendResponse) {
-    if (request.type === 'LOAD_GUARD') {
-        if (loaded) {
-            console.log("Data is already loaded.");
-            sendResponse({'type': 'IGNORE'});
-            return;
-        }
-        loaded = true;
-        // We want to load the data once and only once
-        console.log("Data load requested to the popup");
-        // sendResponse({'type': 'LOAD_DATA'});
-    }
-}
+    switch (request.type) {
+        case 'REQUEST_DATA':
+            // TODO: send the data for the datastore over
+            /*
+             * We need to copy over the suggestions for this day/hour
+             * and all the titles for the urls in that list.
+             */
+            console.log("Trying to send the data over the wire over to the popup");
+            let payload = {'type': 'DATA_UPDATE',
+                           'suggestions': store.getState().suggestions};
+            browser.runtime.sendMessage(payload);
+        default:
+            // Do nothing
+    } }
 
 browser.runtime.onMessage.addListener(handleMessage);
+
+/**********************************************/
+// Loading of the heatmap data starts here
+/**********************************************/
+
+function insert_or_append(url, title, date) {
+    store.dispatch({type: 'ADD_LINK',
+                    day: date.getDay(),
+                    hour: date.getHours(),
+                    url: url,
+                    title: title
+    });
+}
+
+
+function loadHeatmap() {
+    console.log("loadHeatmap starting");
+    var loop_counter = 1;
+    var limit = 500;
+
+    var loop_counter = 0;
+
+    var placesCallable = function(places) {
+        var places_length = places.length;
+        console.log(`Fetched ${places_length} rows from moz_places`);
+
+        places.forEach(function(place) {
+            var placeClause = `WHERE place_id = ${place.id}`;
+            var query = "SELECT id, visit_date, visit_type FROM moz_historyvisits " + placeClause;
+
+            var historyVisitsPromise  = browser.placesdb.query({query: query, params: ['id', 'visit_date', 'visit_type']});
+
+            historyVisitsPromise.then(function(historyVisits) {
+                //console.log(`Fetched ${historyVisits.length} rows from moz_historyvisits`);
+                for (var i = 0; i < historyVisits.length; i++) {
+                    var when = new Date(historyVisits[i].visit_date / (10 ** 3));
+                    insert_or_append(place.url, place.title,when);
+                }
+            }, function(reason) {
+                // TODO: add error handling when scanning the moz_historyvisits
+                // table didn't work
+            });
+        });
+
+        // ******************
+
+        if (places_length == limit) {
+            // Run another iteration over moz_places as we haven't reached the end of the places table.
+            loop_counter += 1;
+            mozPlacesLooper(loop_counter, limit);
+        } else {
+            // We're done - no need to call mozPlacesLooper again
+            // Technically, we aren't *really* done.  There may instances of
+            // placesCallable and mozPlacesLooper that are still running.
+            var sending = browser.runtime.sendMessage({'type': 'DATA_READY'});
+            sending.then(function(msg) {
+                console.log("Success!  The popup was open and notified!");
+            }, function (err) {
+                console.log("IgnorableWarning Can't send message to endpoint");
+            });
+
+        }
+    };
+
+    var mozPlacesLooper = function(loop_counter, limit) {
+        var sql = `SELECT id, title, url FROM moz_places LIMIT ${limit} OFFSET ${loop_counter * limit}`;
+        var mozPlacesPromise = browser.placesdb.query({query: sql, params: ["id", 'title', "url"]});
+
+        mozPlacesPromise.then(placesCallable, function (reason) {
+            /* Querying moz_places failed for some reason */
+        });
+    }
+
+    mozPlacesLooper(loop_counter, limit);
+}
+
+
+// Kick off the data load
+loadGuard();
--- extension/popup/groupspanel.jsx
+++ extension/popup/groupspanel.jsx
@@ -1,77 +1,7 @@
 // This just creates a dummy store for us to connect to.
 
 const store = Redux.createStore(heatmapApp);
-
-console.log("heatmapApp: " + heatmapApp);
-console.log("store: " + store);
-console.log("connect: " + ReactRedux.connect);
-console.log("Provider: " + ReactRedux.Provider);
-
-function insert_or_append(url, title, date) {
-    // console.log("i_or_a: " + url + " -- " + date);
-    store.dispatch({type: 'ADD_LINK',
-                    day: date.getDay(),
-                    hour: date.getHours(),
-                    url: url,
-                    title: title
-    });
-}
-
-
-function loadHeatmap() {
-    console.log("loadHeatmap starting");
-    var loop_counter = 1;
-    var limit = 500;
-
-    var loop_counter = 0;
-
-    var placesCallable = function(places) {
-        var places_length = places.length;
-        console.log(`Fetched ${places_length} rows from moz_places`);
-
-        places.forEach(function(place) {
-            var placeClause = `WHERE place_id = ${place.id}`;
-            var query = "SELECT id, visit_date, visit_type FROM moz_historyvisits " + placeClause;
-
-            var historyVisitsPromise  = browser.placesdb.query({query: query, params: ['id', 'visit_date', 'visit_type']});
-
-            historyVisitsPromise.then(function(historyVisits) {
-                //console.log(`Fetched ${historyVisits.length} rows from moz_historyvisits`);
-                for (var i = 0; i < historyVisits.length; i++) {
-                    var when = new Date(historyVisits[i].visit_date / (10 ** 3));
-                    insert_or_append(place.url, place.title,when);
-                }
-            }, function(reason) {
-                // TODO: add error handling when scanning the moz_historyvisits
-                // table didn't work
-            });
-        });
-
-        // ******************
-
-        if (places_length == limit) {
-            // Run another iteration over moz_places as we haven't reached the end of the places table.
-            loop_counter += 1;
-            mozPlacesLooper(loop_counter, limit);
-        } else {
-            // We're done - no need to call mozPlacesLooper again
-        }
-    };
-
-    var mozPlacesLooper = function(loop_counter, limit) {
-        var sql = `SELECT id, title, url FROM moz_places LIMIT ${limit} OFFSET ${loop_counter * limit}`;
-        var mozPlacesPromise = browser.placesdb.query({query: sql, params: ["id", 'title', "url"]});
-
-        mozPlacesPromise.then(placesCallable, function (reason) {
-            /* Querying moz_places failed for some reason */
-        });
-    }
-
-    mozPlacesLooper(loop_counter, limit);
-}
-
-
-
+var Provider = ReactRedux.Provider;
 
 class Site extends React.Component {
     render() {
@@ -174,8 +104,6 @@ const StoreBackedSuggestedLinks = ReactRedux.connect(
 
 const App = () => React.createElement(StoreBackedSuggestedLinks);
 
-var Provider = ReactRedux.Provider;
-
 ReactDOM.render(
     <Provider store={store}>
         <App />
@@ -184,13 +112,53 @@ ReactDOM.render(
 );
 
 
-// This kicks off the load of the data
-var sending = browser.runtime.sendMessage({'type': 'LOAD_GUARD'});
-sending.then(function(message) {
-    if (message.type === 'LOAD_DATA') {
-        loadHeatmap();
-    }
-},function(error) {
-    console.log(`Error: ${error}`);
-});
+/*
+ * Make a request to background.js for the latest copy of data
+ * we've got.
+ */
+function requestLatestData() {
+    let now = new Date();
+    let day = now.getDay();
+    let hour = now.getHours();
+    let payload = {'type': 'REQUEST_DATA',
+                   'day': day,
+                   'hour': hour};
+    browser.runtime.sendMessage(payload);
+}
 
+
+function insert_or_append(url, title, date) {
+    store.dispatch({type: 'ADD_LINK',
+                    day: date.getDay(),
+                    hour: date.getHours(),
+                    url: url,
+                    title: title
+    });
+}
+
+// Declare and register the message handler
+function handleMessage(request, sender, sendResponse) {
+    switch (request.type) {
+        case 'DATA_READY':
+            requestLatestData();
+            break;
+
+        case 'DATA_UPDATE':
+            let now = new Date();
+            // mapStateToProps will effectively filter the data down to just what we want
+            let storeDict = mapStateToProps(request);
+            var sites = storeDict.sites;
+            console.log(`Appending ${sites.length} rows to popup`);
+            for (var i = 0; i < sites.length; i++) {
+                let row = sites[i];
+                insert_or_append(row.url, row.title, now);
+            }
+            break;
+
+        default:
+    }
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
+
+browser.runtime.sendMessage({'type': 'REQUEST_DATA'});
