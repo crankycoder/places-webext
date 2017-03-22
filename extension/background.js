bd72c7e3eb08239b8900f645acad37aeabe3563c
Victor Ng <victor@crankycoder.com>
2017-03-22 12:59:47 -0400

    refactored the reducer into the background.js script

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,24 @@
+if (Redux === undefined) {
+    console.log("Redux is not loaded yet");
+} else {
+    console.log("Redux is available!");
+
+}
+
+const store = Redux.createStore(heatmapApp);
+
+console.log("background.js heatmapApp: " + heatmapApp);
+console.log("background.js store: " + store);
+
 var loaded = false;
 
 function handleMessage(request, sender, sendResponse) {
+    // We have to wait for the first click to invoke the Redux code because JS
+    // is stupid and we have no guarantees about which code is actually loaded
+    console.log("In background.js .. Redux namespace: " + Redux);
+}
+
+function oldhandleMessage(request, sender, sendResponse) {
     if (request.type === 'LOAD_GUARD') {
         if (loaded) {
             console.log("Data is already loaded.");
@@ -10,7 +28,7 @@ function handleMessage(request, sender, sendResponse) {
         loaded = true;
         // We want to load the data once and only once
         console.log("Data load requested to the popup");
-        sendResponse({'type': 'LOAD_DATA'});
+        // sendResponse({'type': 'LOAD_DATA'});
     }
 }
 
